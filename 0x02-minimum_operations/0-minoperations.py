4b3e0dca2b5f1c328b1ce602f862f2de77dd1d55
nobleenia <neluwah@gmail.com>
2024-04-25 15:59:08 +0000

    First push

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This module contains a function that calculates
+the minimum number of operations needed
+to reach exactly n 'H' characters in a text file
+using only Copy All and Paste operations.
+"""
+
+def minOperations(n):
+    """
+    Determine the minimum number of operations
+    needed to result in exactly n 'H' characters.
+
+    Parameters:
+    n (int): The target number of 'H' characters.
+
+    Returns:
+    int: The minimum number of operations required,
+    or 0 if n cannot be achieved.
+    """
+    if n < 2:
+        return 0
+
+    operations = 0
+    current_factor = 2
+
+    while n > 1:
+        while n % current_factor == 0:
+            operations += current_factor
+            n //= current_factor
+        current_factor += 1
+
+    return operations
+
+if __name__ == "__main__":
+    print("Min # of operations to reach 4 char:", minOperations(4))
+    print("Min # of operations to reach 12 char:", minOperations(12))
