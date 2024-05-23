180eb8cd5aae51c3609eba4e975202c1fbece89a
nobleenia <neluwah@gmail.com>
2024-05-23 08:21:24 -0700

    Update: First Commit

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.log('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  characters.forEach((character) => {
+    request(character, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  });
+});
